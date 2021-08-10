6d0f954db49a7f5466e839419e4e02ed305571ac
Sachin Kumar Rajput <skr571999@gmail.com>
2021-08-10 19:19:39 +0530

    initial commit

---
+++ app1/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+#sign up
+from .models import *
+
+
+admin.site.register(prediction1)
---
+++ app1/url.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('about', views.about, name='about'),
+    path('contactus', views.contactus, name='contactus'),
+   
+    path('predication1', views.predication1, name='predication1')
+    
+
+
+]
